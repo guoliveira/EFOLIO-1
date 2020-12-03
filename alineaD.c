8bf37717bdc48c777bb444e0e60b71d18836ac53
Luis Guilherme Sousa de Oliveira <lgsoliveira@gmail.com>
2020-12-03 15:18:51 +0000

    Com JA

--- alineaD.c
+++ alineaD.c
@@ -7,50 +7,43 @@
 #include <stdbool.h>
 
 int verificar_posicao (char casa[])
-  {
+{
     if(strlen(casa)>2)
-        return 0 ; 
+        return 0; 
+    // Avaliar se a primeira char fica entre a e f
+    else if (casa[0]<'a'||casa[0]>'f')
+        return 0;  
+    // Avaliar se a segunda char fica entre 1 e 5
+    else if (casa[1]<'1'||casa[1]>'5')
+        return 0; 
     else
-      {
-      // Avaliar se a primeira char fica entre a e f
-      if (casa[0]<'a'||casa[0]>'f')
-        return 0 ;  
-      else
-        {
-        // Avaliar se a segunda char fica entre 1 e 5
-         if (casa[1]<'1'||casa[1]>'5')
-          {
-          return 0; 
-          }
-         else
-          return 1; 
-        }
-      }
-  }
+        return 1; 
+}
+
 
 //Função que recebe um string com espacos, separa-o e devolve um vector string
 char** separar_casas(char casas[])
 {
-      static char *localizacao[80];
-      char *apontador = strtok(casas, " ");
-      int c;
+    static char *localizacao[100];
+    char *casa_sem_espacos = strtok(casas, " ");
+    int c;
 
-      c=0;
-      while (apontador != NULL)
+    c=0;
+    while (casa_sem_espacos != NULL)
         {
-        localizacao[c]=apontador;
+        localizacao[c]=casa_sem_espacos;
         c++;
-        apontador = strtok(NULL, " ");
+        casa_sem_espacos = strtok(NULL, " ");
         }
-      return localizacao;
+    return localizacao;
 
 }   
 
 char* criar_tabuleiro()
 {
-   static char posicoes[50];
-   int i;
-   for (i=0;i<36;i++)
+    static char posicoes[50];
+    int i;
+    for (i=0;i<36;i++)
       posicoes[i]='.';
     
     return posicoes;
@@ -58,16 +51,16 @@ char* criar_tabuleiro()
 
 int converte_localizacao_em_numero(char casa[])
 {
-  int i;
-  char *numero[50]={"0","1","2","3","4","5"};
-  char *letra[50]={"a","b","c","d","e","f"};
+    int i;
+    char *numero[50]={"0","1","2","3","4","5"};
+    char *letra[50]={"a","b","c","d","e","f"};
   
-  if (verificar_posicao(casa)==1)
-    for (i=0;i<36;i++)
-      if((casa[0]==*letra[i%6])&&(casa[1]==*numero[5-i/6]))
-        return i;
+    if (verificar_posicao(casa)==1)
+      for (i=0;i<36;i++)
+        if((casa[0]==*letra[i%6])&&(casa[1]==*numero[5-i/6]))
+          return i;
   
-  return -1;
+    return -1;
 }
 
 
@@ -86,18 +79,20 @@ void inserir_pecas(char casas[],char tabuleiro[])
                 }       
 }
 
-int validar_movimento_peca(char *de, char *para, char tabuleiro[])
+int validar_movimento_peca(char *origem, char *destino, char tabuleiro[])
 {
-  int de_int=converte_localizacao_em_numero(de);
-  int para_int=converte_localizacao_em_numero(para);
+  int de_int=converte_localizacao_em_numero(origem);
+  int para_int=converte_localizacao_em_numero(destino);
 
-  if (verificar_posicao(de)==0||verificar_posicao(para)==0)
+  if (verificar_posicao(origem)==0||verificar_posicao(destino)==0)
     return 0;
   else if (tabuleiro[de_int]=='.')
     return 0;
   else if (tabuleiro[de_int]=='p')
       {
-      if (tabuleiro[para_int]=='P'&&( para_int-de_int==7||para_int-de_int==5))
+      if (tabuleiro[para_int]=='R')
+        return -1;
+      else if (tabuleiro[para_int]=='P'&&( para_int-de_int==7||para_int-de_int==5))
         return 1;
       else if (tabuleiro[de_int]=='.'&&para_int-de_int==6)
         return 1;
@@ -106,7 +101,9 @@ int validar_movimento_peca(char *de, char *para, char tabuleiro[])
       }
   else if (tabuleiro[de_int]=='P')
       {
-      if (tabuleiro[para_int]=='p'&&( de_int-para_int==7||de_int-para_int==5))
+      if (tabuleiro[para_int]=='r')
+        return -1;
+      else if (tabuleiro[para_int]=='p'&&( de_int-para_int==7||de_int-para_int==5))
         return 1;
       else if (tabuleiro[para_int]=='.'&&de_int-para_int==6)
         return 1;
@@ -115,7 +112,9 @@ int validar_movimento_peca(char *de, char *para, char tabuleiro[])
       }
   else if (tabuleiro[de_int]=='r')
     {
-    if (tabuleiro[para_int]=='p')
+    if (tabuleiro[para_int]=='R'&&(de_int-para_int==1||para_int-de_int==1||de_int-para_int==6||para_int-de_int==6||de_int-para_int==7||para_int-de_int==7||de_int-para_int==5||para_int-de_int==5))
+      return -1;
+    else if (tabuleiro[para_int]=='p')
       return 0;
     else if (de_int-para_int==1||para_int-de_int==1||de_int-para_int==6||para_int-de_int==6||de_int-para_int==7||para_int-de_int==7||de_int-para_int==5||para_int-de_int==5)
       return 1;
@@ -124,7 +123,9 @@ int validar_movimento_peca(char *de, char *para, char tabuleiro[])
     }
   else if (tabuleiro[de_int]=='R')
     {
-    if (tabuleiro[para_int]=='P')
+    if (tabuleiro[para_int]=='r'&&(de_int-para_int==1||para_int-de_int==1||de_int-para_int==6||para_int-de_int==6||de_int-para_int==7||para_int-de_int==7||de_int-para_int==5||para_int-de_int==5))
+      return -1;
+    else if (tabuleiro[para_int]=='P')
       return 0;
     else if (de_int-para_int==1||para_int-de_int==1||de_int-para_int==6||para_int-de_int==6||de_int-para_int==7||para_int-de_int==7||de_int-para_int==5||para_int-de_int==5)
       return 1;
@@ -136,33 +137,105 @@ int validar_movimento_peca(char *de, char *para, char tabuleiro[])
 
 }
 
+void regras_JA(char tabuleiro[])
+{
+  int rei[2];
+  int peao[8][2];
+  int i,j=0,k=0;
+
+    // As casas pretas estão no [0] e o branco no [1]
+
+  for (i=0;i<36;i++)
+    if (tabuleiro[i]=='r')
+      rei[0]=i;
+    else if (tabuleiro[i]=='R')
+      rei[1]=i;
+    else if (tabuleiro[i]=='p')
+      {
+      peao[j][0]=i;
+      j++;
+      }
+    else if (tabuleiro[i]=='P')
+      {
+      peao[k][1]=i;
+      k++;
+      }
+    else 
+      tabuleiro[i]='.';
+
+    for (j=0;j<6;j++)
+      for (k=0;k<6;k++)
+        {
+        if (peao[k][1]-peao[j][0]==6)
+          {
+          if (tabuleiro[peao[j][0]-1]=='p')
+            {
+            tabuleiro[peao[j][0]-1]='.';
+            tabuleiro[peao[k][1]]='p';
+            printf("");
+            }
+           else {
+            tabuleiro[peao[j][0]+1]='.';
+            tabuleiro[peao[k][1]]='p';
+            printf("");
+           }
+          }
+        }
+
+    for (j=0;j<6;j++)
+        if (rei[1]-peao[j][0]==6)
+        {
+            tabuleiro[peao[j][0]]='.';
+            tabuleiro[peao[j][0]+5]='p';
+        }
+
+}
+
+
 int mover_pecas(char casas[], char tabuleiro[])
 {
   int i;
   static int jogadas;
   char peca;
-  char *de,*para;
+  char *origem,*destino;
 
+  
+  
+  
   for (i=14;separar_casas(casas)[i]!=0;i++)
     {
-    de=separar_casas(casas)[i];
-    para=(separar_casas(casas)[i+1]);
-
-    if (validar_movimento_peca(de,para,tabuleiro)==0)
-     break;
-    else
+    if (separar_casas(casas)[i+1]!=0)
     {
-    peca=tabuleiro[converte_localizacao_em_numero(de)];
-    tabuleiro[converte_localizacao_em_numero(de)]='.';
-    tabuleiro[converte_localizacao_em_numero(para)]=peca;
+    if (separar_casas(casas)[i][0]=='J')
+      {
+      regras_JA(tabuleiro);
+      jogadas++;
+      }
+
+    else if (validar_movimento_peca(separar_casas(casas)[i],separar_casas(casas)[i+1],tabuleiro)==0)
+     break;
+    else     
+    {
+    origem=separar_casas(casas)[i];
+    destino=(separar_casas(casas)[i+1]);
+
+    peca=tabuleiro[converte_localizacao_em_numero(origem)];
+    tabuleiro[converte_localizacao_em_numero(origem)]='.';
+    tabuleiro[converte_localizacao_em_numero(destino)]=peca;
     i++;
     jogadas++;
+    if (validar_movimento_peca(origem,destino,tabuleiro)==-1)
+        break;
     }
+    }
+    else
+      break;
     }
 
 return jogadas;
 
 }
+
 
 char* imprimir_output(char tabuleiro[])
 {          
@@ -186,7 +259,7 @@ char* imprimir_output(char tabuleiro[])
 
 
 int main(void) {
-    char casas[100];
+    char casas[200];
     char *tabuleiro;
     int j;
     int jogadas;
